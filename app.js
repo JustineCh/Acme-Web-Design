88f55ea7a1937e29e10e45c4a623a06a95506ec6
Justyna Chrostek <justyna.maria.chrostek@gmail.com>
2021-01-20 17:49:20 +0100

    Navbar - @media with js

---
+++ app.js
@@ -0,0 +1,4 @@
+document.getElementById("hamburger-button").addEventListener("click", () => {
+  document.getElementById("navbar-list").classList.toggle("mobile-navbar");
+  document.getElementById("hamburger-icon").classList.toggle("fa-times");
+});
